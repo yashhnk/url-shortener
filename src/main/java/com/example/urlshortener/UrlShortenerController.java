319f3ca9845518d3a8f26cef1b7d35e45656d52e
Dev Ops <yashkhose25@gmail.com>
2025-06-30 21:04:02 +0530

    added import responseentity

--- src/main/java/com/example/urlshortener/UrlShortenerController.java
+++ src/main/java/com/example/urlshortener/UrlShortenerController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api")
