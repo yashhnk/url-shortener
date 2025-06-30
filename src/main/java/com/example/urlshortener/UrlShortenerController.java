34a062ab51729bcd5f56d6e493e3a9cc0ad54731
Dev Ops <yashkhose25@gmail.com>
2025-06-30 20:13:06 +0530

    changed redirect mapping

--- src/main/java/com/example/urlshortener/UrlShortenerController.java
+++ src/main/java/com/example/urlshortener/UrlShortenerController.java
@@ -2,7 +2,8 @@ package com.example.urlshortener;
 
 import io.micrometer.core.annotation.Counted;
 import org.springframework.web.bind.annotation.*;
-
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -19,10 +20,13 @@ public class UrlShortenerController {
         map.put(shortId, originalUrl);
         return shortId;
     }
-
-    @GetMapping("/retrieve/{shortId}")
-    @Counted("retrieve_requests_total")
-    public String retrieve(@PathVariable String shortId) {
-        return map.getOrDefault(shortId, "URL Not Found");
-    }
+    @GetMapping("/{shortId}")
+    public void redirect(@PathVariable String shortId, HttpServletResponse response) throws IOException {
+    	String originalUrl = map.get(shortId);
+    	if (originalUrl != null) {
+        	response.sendRedirect(originalUrl);
+    	} else {
+        	response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    	}
+   }
 }
